d282baa5a9b76bbddffce9e3aad5f78c3a501f3b
h-reader <super.black.jack777@gmail.com>
2018-05-07 14:39:23 +0900

    Amend resource pass

--- dist/precache-manifest.7ec88454f052f34528e1aad479215bc0.js
+++ dist/precache-manifest.7ec88454f052f34528e1aad479215bc0.js
@@ -1,15 +1,15 @@
 self.__precacheManifest = [
   {
     "revision": "1d4e3ee6e7b6208656f9",
-    "url": "js/vendor.1d4e3ee6.js"
+    "url": "./js/vendor.1d4e3ee6.js"
   },
   {
     "revision": "bb41d6d80c1fdaad6874",
-    "url": "js/manifest.bb41d6d8.js"
+    "url": "./js/manifest.bb41d6d8.js"
   },
   {
     "revision": "027b8d70f59633232901",
-    "url": "js/app.027b8d70.js"
+    "url": "./js/app.027b8d70.js"
   },
   {
     "revision": "b93802fc9c5ca00c1775ef5ebb83bd87",
@@ -17,6 +17,6 @@ self.__precacheManifest = [
   },
   {
     "revision": "027b8d70f59633232901",
-    "url": "css/app.3db86a4e.css"
+    "url": "./css/app.3db86a4e.css"
   }
 ];
